70dbac71b4af613ae0225fc93ffc72158ebd8d5a
Jay <jay@vishs-mbp.lan>
2022-12-27 14:46:18 -0500

    Added some new test files and added a 50/50 chance on Math Is Hard file.

--- MathIsHard.py
+++ MathIsHard.py
@@ -1,8 +1,17 @@
+import random
+import time
 wonderBoyAllowance=10
 newCape=20
 print("That new cape sure is shiny. I wonder if you can afford it...")
-if wonderBoyAllowance>newCape:
-    print("Congrats! You have enough to buy that new cape!")
+time.sleep(1.5)
+if wonderBoyAllowance<newCape:
+    print("You are broke. Maybe if you ask nicely Wonder Dad will give you a raise...")
+if bool(random.getrandbits(1)):
+    wonderBoyAllowance=25
+    print("raise given")
+    time.sleep(1.5)
+    print("Congrats! You now have a real cape!")
 else:
+    print("raise rejected")
+    time.sleep(1.5)
     print("Looks like you'll have to keep wearing that towel as a cape...")
-    print("Maybe if you ask nicely Wonder Dad will give you a raise...")
