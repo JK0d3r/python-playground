b95147495ec168475c686359d59c9b149a8483e1
JK0d3r <jay.khalipe@gmail.com>
2024-11-10 16:41:20 -0500

    expanded more on the snake game. Added the dimensions, spawnability of food. Fixed a bug where the display went below the bottom of my screen

--- snakeGame.py
+++ snakeGame.py
@@ -15,7 +15,15 @@ class Snake:
     pass
 
 class Food:
-    pass
+    
+    def __init__(self):
+        
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE)-1) * SPACE_SIZE
+
+        self.coordintes = [x, y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
 def nextTurn():
     pass
@@ -50,9 +58,11 @@ screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
 x = int((screen_width/2) - (window_width/2))
-y = int((screen_width/2) - (screen_height/2))
+y = int((screen_width/2) - (screen_height/2)-150)
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+snake = Snake()
+food = Food()
 
 window.mainloop()
